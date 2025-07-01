433c665b5b8f3950a9d5e71b898b14e683012140
Dat Le <ldat0909@gmail.com>
2025-07-01 19:45:56 +0700

    Fix port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import app from './app'
 import { initRecurringTransactionJob } from './cron/recurringJob';
 import { initCheckBudgetAlert } from './cron/checkBudgetAlert';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 initRecurringTransactionJob();
 initCheckBudgetAlert();
