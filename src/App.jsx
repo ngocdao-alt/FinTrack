8aaab1b1643f47711c17098862f9617c5c878fc9
ngocdao-alt <daongocnguyen27092004@gmail.com>
2025-07-01 19:47:54 +0700

    Done Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
+// src/App.jsx
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LoginPage from './Pages/LoginPage'
-import { Route, Routes } from 'react-router-dom'
-
+import LandingPage from './Pages/LandingPage'
+import Sidebar from './components/SideBarComponent'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)      
+  const location = useLocation()
+
+  const hideSidebar = location.pathname === '/login' || location.pathname === '/register'
 
   return (
-    <>
-    <Routes>
-      <Route path="/login" element= {<LoginPage/>}/>
-      
-    </Routes>
-    </>
+    <div className="flex">
+      {!hideSidebar && <Sidebar />}
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
+        </Routes>
+      </div>
+    </div>
   )
 }
 
