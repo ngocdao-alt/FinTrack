9a639166cac007c27ed1834a5d0fd234b81a8f42
Dat Le <ldat0909@gmail.com>
2025-07-03 17:49:10 +0700

    Fix conflict at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import LandingPage from "./Pages/LandingPage";
-<<<<<<< HEAD
 import Sidebar from "./components/SideBarComponent";
 import DashboardPage from "./pages/DashboardPage";
 import Header from "./components/Header";
@@ -12,14 +11,6 @@ import TransactionPage from "./pages/TransactionPage";
 import StatPage from "./pages/StatPage";
 import SettingPage from "./pages/SettingPage";
 
-=======
-import DashboardPage from "./Pages/DashboardPage";
-import Header from "./components/Header";
-import BudgetPage from "./Pages/BudgetPage";
-import TransactionPage from "./Pages/TransactionPage";
-import StatPage from "./Pages/StatPage";
-import SettingPage from "./Pages/SettingPage";
->>>>>>> origin/feature/dao-transactionPage
 function App() {
   const [count, setCount] = useState(0);
   const location = useLocation();
@@ -36,11 +27,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/budget" element={<BudgetPage />} />
-<<<<<<< HEAD
-          <Route path="/transaction" element={<TransactionPage />} />
-=======
           <Route path="/transactions" element={<TransactionPage />} />
->>>>>>> origin/feature/dao-transactionPage
           <Route path="/stat" element={<StatPage />} />
           <Route path="/setting" element={<SettingPage />} />
         </Routes>
